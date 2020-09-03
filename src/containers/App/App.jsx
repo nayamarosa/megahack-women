2197c8215d3471d950e7500fe7ea7c2d8781ec21
NayamaRosa <falecom@nayama.com.br>
2020-09-03 14:16:34 -0300

    app container

---
+++ src/containers/App/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from '../../routes';
+import './App.css';
+
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes />
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+export default App;
