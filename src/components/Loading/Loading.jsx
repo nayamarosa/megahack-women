617dbe3d0fd5df8fc021078ca090fe3546a147a1
NayamaRosa <falecom@nayama.com.br>
2020-09-04 15:39:34 -0300

    loading component

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Loading.css';
+
+const Loading = () => {
+  return (
+    <div className="loading">
+      <div className="lds-ellipsis">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  )
+}
+
+export default Loading;
