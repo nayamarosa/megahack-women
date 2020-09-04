ccb274a0c2a86cf5d7553b9894c7560442e7c9b8
NayamaRosa <falecom@nayama.com.br>
2020-09-04 10:39:50 -0300

    [entrada][fetch] entrada route appearing data

--- src/routes/Entradas/Entradas.jsx
+++ src/routes/Entradas/Entradas.jsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import DataTable from '../../containers/DataTable';
 
 const Entradas = () => {
+  const [list, setList] = useState([]);
+  
+  useEffect(() => {
+    const getData = async() => {
+      await fetch(' https://demo6507195.mockable.io/megahack')
+        .then(res => res.json())
+        .then(data => setList(data.products))
+    }
+    getData();
+  }, [])
+  
   return (
-    <p>Entradas</p>
+    <main>
+      <h2>Minhas entradas</h2>
+      <table>
+        <DataTable list={ list } />
+      </table>
+    </main>
   )
 }
 
