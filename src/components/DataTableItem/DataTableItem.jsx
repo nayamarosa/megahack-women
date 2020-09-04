a0fe2bd8776b0077a160ac44fdc94fc8f5ef4b40
NayamaRosa <falecom@nayama.com.br>
2020-09-04 15:41:04 -0300

    entries and outs style on data table showing

--- src/components/DataTableItem/DataTableItem.jsx
+++ src/components/DataTableItem/DataTableItem.jsx
@@ -1,22 +1,41 @@
 import React from 'react';
 
 const DataTableItem = ({ item }) => {
-  const { name, price, sales } = item;
+  const { name, price, sales, buys } = item;
+  const path = window.location.pathname;
 
   return (
     <>
-    {sales.map((sale, index) => {
-      return (
-        <tr key={`${index}-${name}`}>
-          <td>{sale.saleDate}</td>
-          <td>{name}</td>
-          <td>{price.sale}</td>
-          <td>{sale.saleQuantity}</td>
-          <td>{sale.saleTotal}</td>
-        </tr>
+    {
+    path === "/minhas-entradas"
+    ? sales.map((sale, index) => {
+          return (
+            <tr key={`${index}-${name}`} className="data__table-item">
+              <td>{sale.saleDate}</td>
+              <td>{name}</td>
+              <td>{price.sale}</td>
+              <td>{sale.saleQuantity}</td>
+              <td>{sale.saleTotal}</td>
+            </tr>
+          )
+        }
       )
-    }
-    )}
+    : path === "/minhas-saidas"
+      ? buys.map((buy, index) => {
+        return (
+          <tr key={`${index}-${name}`} className="data__table-item">
+            <td>{buy.buyDate}</td>
+            <td>{name}</td>
+            <td>{price.purchase}</td>
+            <td>{buy.buyQuantity}</td>
+            <td>{buy.buyTotal}</td>
+          </tr>
+        )
+      }
+    )
+      : false
+  }
+    
     </>
   )
 }
--- src/containers/DataTable/DataTable.jsx
+++ src/containers/DataTable/DataTable.jsx
@@ -4,18 +4,20 @@ import './DataTable.css';
 import DataTableItem from '../../components/DataTableItem';
 
 const DataTable = ({ list }) => {
+  const path = window.location.pathname;
+
   return (
     <>
-      <thead>
+      <thead className="data__table-head">
         <tr>
           <td>Data</td>
-          <td>Produto/ Serviço</td>
-          <td>Valor unitário</td>
+          <td>Produto/ <br/>Serviço</td>
+          <td>Valor <br/>unitário</td>
           <td>Qntde.</td>
-          <td>Valor total</td>
+          <td>Valor <br/>total</td>
         </tr>
       </thead>
-      <tbody>
+      <tbody className={`data__table-body ${path === "/minhas-entradas" ? "data__table-body--entries" : ""} ${path === "/minhas-saidas" ? "data__table-body--outs" : ""}`}>
         {list.map(item => 
           <DataTableItem 
             key={item.code}
